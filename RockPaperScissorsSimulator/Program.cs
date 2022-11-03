0e56974a78dd2cea6489c6d3e860cc7e115a40d5
Francis Trynka <52320368+iPigus@users.noreply.github.com>
2022-11-03 15:59:42 +0100

    Update Program.cs
    That's how it should be done more efficiently

--- RockPaperScissorsSimulator/Program.cs
+++ RockPaperScissorsSimulator/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -10,73 +10,26 @@ namespace RockPaperNozyc
     {
         static void Main(string[] args)
         {
-            int x = 10000 - 3;
-            int win;
-            int draw;
-            int lose;
-            lose = 1;
-            draw = 1;
-            win = 1;
+            int x = 10000;
+            int win = 0;
+            int draw = 0;
+            int lose = 0;
 
-
-
-            Console.WriteLine("Pa na to");
             Random rnd = new Random();
-            for (int y = 1; y <= x; y++)
+            for (int y = 0; y <= x; y++)
             {
-                int rng = rnd.Next(1, 4);
-                int rng2 = rnd.Next(1, 4);
-                if (rng == rng2)
+                switch(rnd.Next(0,3))
                 {
-                    draw++;
+                    case 0: win++; break;
+                    case 1: draw++; break;
+                    case 2: lose++; break;
                 }
-                if (rng == 1)
-                {
-                    if (rng2 == 2)
-                    {
-                        win++;
-                    }
-                    if (rng2 == 3)
-                    {
-                        lose++;
-                    }
-                }
-                if (rng == 2)
-                {
-                    if (rng2 == 3)
-                    {
-                        win++;
-                    }
-                    if (rng2 == 1)
-                    {
-                        lose++;
-                    }
-                }
-                if (rng == 3)
-                {
-                    if (rng2 == 1)
-                    {
-                        win++;
-                    }
-                    if (rng2 == 2)
-                    {
-                        lose++;
-                    }
-                }
-
-
-
-
             }
-            Console.WriteLine("tyle bylo wygranych");
-            Console.WriteLine(win);
-            Console.WriteLine("tyle bylo remisow");
-            Console.WriteLine(draw);
-            Console.WriteLine("tyle bylo przegranych");
-            Console.WriteLine(lose);
+            Console.WriteLine("tyle bylo wygranych\n" + win);
+            Console.WriteLine("tyle bylo remisow\n" + draw);
+            Console.WriteLine("tyle bylo przegranych\n" + lose);
             int c = win + draw + lose;
-            Console.WriteLine("tyle bylo gier jbc");
-            Console.WriteLine(c);
+            Console.WriteLine("tyle bylo gier jbc\n" + c);
         }
 
 
